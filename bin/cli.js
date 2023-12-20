148143eeadd0b2c4bd9992b8d6f169adcb266d08
yoad208 <yoad208@gmail.com>
2023-12-21 01:15:36 +0200

    commit

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const appName = process.argv[2]
+
+if (appName.length === 0) {
+    console.error('Please provide a name for the application')
+    process.exit(1)
+}
+
+const { execSync } = require('child_process')
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/yoadazani/react-template.git ${appName}`
+const installDependencies = `cd ${appName} && npm install`
+
+const runCommand = (command) => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit' })
+    } catch (error) {
+        console.error(`Failed to execute ${command}`, error)
+        return false
+    }
+    return true
+}
+
+const clone = runCommand(gitCheckoutCommand)
+if (!clone) process.exit(1)
+
+execSync(`Clone application from registry...`)
+
+const install = runCommand(installDependencies)
+if (!install) process.exit(1)
+
+console.log(`Application ${appName} created successfully`)
+console.log(`Run 'cd ${appName} && npm start' to start the application`)
+process.exit(0)
